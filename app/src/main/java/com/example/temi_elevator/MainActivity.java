08ee652853e4fd7f83800f170d11c4733cc4ab65
0treble <bassguitarplayer97@gmail.com>
2024-06-04 18:24:15 +0200

    Braingame expanded: > timeout when user doesnt answer > user can ask to repeat a question

--- app/src/main/java/com/example/temi_elevator/MainActivity.java
+++ app/src/main/java/com/example/temi_elevator/MainActivity.java
@@ -323,6 +323,7 @@ public class MainActivity extends AppCompatActivity implements
         showTranscription("\nDEGBUG: In der analyzeVoiceCommand...\n");
         chooseCurrentSequence();
     }
+    // testkommentar
 
     public void stopCurrentSequence()
     {
