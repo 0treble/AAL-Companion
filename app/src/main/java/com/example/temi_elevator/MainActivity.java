4ded5556f0424f70e1833ac6809d93a6feaafb38
OrlandoGtzL <erce.gtz@gmail.com>
2024-07-15 14:00:39 +0200

    Improved buttons visuals when pressed. Error relating sequences needs to be fixed on main activity

--- app/src/main/java/com/example/temi_elevator/MainActivity.java
+++ app/src/main/java/com/example/temi_elevator/MainActivity.java
@@ -63,7 +63,7 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity implements
+public class MainActivity extends AppCompatActivity implements 
         OnRobotReadyListener,
         Robot.AsrListener,
         Robot.TtsListener,
